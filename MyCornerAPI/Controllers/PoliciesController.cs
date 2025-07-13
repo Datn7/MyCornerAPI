1350d838ed71108d796f234169e22d6995e94a46
Datn7 <datunashvili72@gmail.com>
2025-07-13 22:36:05 +0400

    Added poicy controller and dto

---
+++ MyCornerAPI/Controllers/PoliciesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyCornerAPI.Controllers
+{
+    public class PoliciesController : ControllerBase
+    {
+        private readonly IPolicyService _policyService;
+
+        public PoliciesController(IPolicyService policyService)
+        {
+            _policyService = policyService;
+        }
+
+        [HttpGet("user/{userId}")]
+        public async Task<IActionResult> GetUserPolicies(int userId)
+        {
+            var policies = await _policyService.GetPoliciesByUserIdAsync(userId);
+            return Ok(policies);
+        }
+    }
+}
---
+++ MyCornerAPI/Models/Dtos/PolicyDto.cs
@@ -0,0 +1,13 @@
+﻿namespace MyCornerAPI.Models.Dtos
+{
+    public class PolicyDto
+    {
+        public int PolicyId { get; set; }
+        public string PolicyNumber { get; set; }
+        public string PolicyType { get; set; }
+        public string Coverage { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Status { get; set; }
+    }
+}
